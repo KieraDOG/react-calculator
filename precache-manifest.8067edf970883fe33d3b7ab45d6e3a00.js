aa818af2b96df30bb47a65c3439c6852dd445cb2
Long Zhao <zlong@outlook.com>
2020-08-28 14:33:47 +1000

    Updates

---
+++ precache-manifest.8067edf970883fe33d3b7ab45d6e3a00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77006326ca7bc8738d1ae665faafe473",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "2f539a109106233e93f9",
+    "url": "/react-calculator/static/css/main.b30e35bf.chunk.css"
+  },
+  {
+    "revision": "d62073050b973db228ca",
+    "url": "/react-calculator/static/js/2.10a13db1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-calculator/static/js/2.10a13db1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f539a109106233e93f9",
+    "url": "/react-calculator/static/js/main.34f15193.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
